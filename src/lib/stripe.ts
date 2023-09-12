e47b17816e9f60bd6d9b7dc76567ee1506300434
YanCardoso <yancard7@gmail.com>
2023-09-12 00:22:28 -0300

    Stripe SSG

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe'
 export const stripe = new Stripe(process.env.PRIVATE_STRIPE_KEY!, {
   apiVersion: '2023-08-16',
   appInfo: {
-    name: 'Y Shop ',
+    name: 'Y Shop',
   },
 })
